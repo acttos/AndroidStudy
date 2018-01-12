f9a89e952ce1cb5e4ab0836c80ac09f6fa7ddbfd
majinshou <majinshou@gmail.com>
2018-01-12 16:42:00 +0800

    Add the Button into MainActivity, add OnClickListener to these Buttons.

--- app/src/main/java/org/acttos/androidstudy/MainActivity.java
+++ app/src/main/java/org/acttos/androidstudy/MainActivity.java
@@ -2,12 +2,33 @@ package org.acttos.androidstudy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button upButton = findViewById(R.id.up_button);
+        upButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "UpButton clicked.");
+            }
+        });
+
+        Button downButton = findViewById(R.id.down_button);
+        downButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "DownButton clicked");
+            }
+        });
+
+
     }
 }
